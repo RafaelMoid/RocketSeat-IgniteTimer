a1b1ddc4f38d7d9a02c0e9f2d73b026072652522
rafael <rafaelvrcr@gmail.com>
2024-08-20 19:23:24 -0300

    SPA aplication timer until class Configurando temas

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './assets/Button';
+import { defaultTheme } from './styles/themes/default';
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant={'primary'}/>
+      <Button variant={'secondary'}/>
+      <Button variant={'danger'}/>
+      <Button />
+    </ThemeProvider>
+  )
+}
---
+++ src/assets/Button.styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const ButtonVariants = {
+  primary: "purple",
+  secondary: "Blue",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  border: 0;
+  border-radius: 4px;
+  margin: 8px;
+
+  background-color: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /* ${(props) => {
+    return `background-color: ${ButtonVariants[props.variant]}`;
+  }} */
+`;
