b4d63d504c89e8dc00c69f212d0f65e5f79be3d1
rafael <rafaelvrcr@gmail.com>
2024-08-21 15:43:33 -0300

    React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { ThemeProvider } from 'styled-components';
-import { Button } from './assets/Button';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant={'primary'}/>
-      <Button variant={'secondary'}/>
-      <Button variant={'danger'}/>
-      <Button />
+      <BrowserRouter>
+        <Router />
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
