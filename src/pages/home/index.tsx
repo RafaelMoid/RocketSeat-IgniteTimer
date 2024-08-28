792b0ef5e747c8a39688c0b068b7dffcc9bc6e0a
rafael <rafaelvrcr@gmail.com>
2024-08-28 10:46:20 -0300

    melhorando os inputs

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -18,11 +18,22 @@ export function Home() {
           <TaskInput
             placeholder="Defina qual será a sua tarefa"
             id="task"
+            list="task-suggestions"
             type="text"
           />
 
+          <datalist id="task-suggestions">
+            <option value="x"></option>
+          </datalist>
+
           <label htmlFor="minutsAmount">durante</label>
-          <MinutsAmountInput id="minutsAmount" type="number" placeholder="00" step={5}/>
+          <MinutsAmountInput
+            id="minutsAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            max={999}
+          />
 
           <span>minutos.</span>
         </FormContainer>
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -51,10 +51,14 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
     flex: 1;
+
+    &::-webkit-calendar-picker-indicator {
+        display: none !important;
+    }
 `;
 
 export const MinutsAmountInput = styled(BaseInput)`
-  width: 4rem;
+  width: 5rem;
 `;
 
 export const CountdownContainer = styled.div`
