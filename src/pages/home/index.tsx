5aa2a39ae2e9ac45b78724d4242e7d8a47175e86
rafael <rafaelvrcr@gmail.com>
2024-08-29 10:50:24 -0300

    mudando o title da pagina dinamicamente com o contador

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,8 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as zod from 'zod';
-import { differenceInSeconds } from 'date-fns'
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { differenceInSeconds } from "date-fns";
 
 import {
   CountdownContainer,
@@ -15,82 +15,91 @@ import {
 } from "./styles";
 import { useEffect, useState } from "react";
 
-const  newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informa a tarefa'),
-  minutesAmount: zod.number().min(5, 'O cliclo precisa ser de no mínimo 5 minutos.').max(90, 'O cliclo precisa ter de no máximo 90 minutos.'),
-})
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informa a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O cliclo precisa ser de no mínimo 5 minutos.")
+    .max(90, "O cliclo precisa ter de no máximo 90 minutos."),
+});
 
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 interface Cycle {
-  id: string
-  task: string
-  minutesAmount: number
-  startDate: Date
+  id: string;
+  task: string;
+  minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
-
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
-      task: '',
+      task: "",
       minutesAmount: 0,
     },
   });
 
-  const activeCycle = cycles.find((cycle)=> cycle.id === activeCycleId)
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
-  useEffect(()=>{
-    let interval : number
+  useEffect(() => {
+    let interval: number;
 
-    if(activeCycle) {
-      interval = setInterval(()=>{
+    if (activeCycle) {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
-        )
-      },1000)
+        );
+      }, 1000);
     }
 
     return () => {
-      clearInterval(interval)
-    }
-  }, [activeCycle])
+      clearInterval(interval);
+    };
+  }, [activeCycle]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-
-    const newCycle : Cycle = {
+    const newCycle: Cycle = {
       id: String(new Date().getTime()),
       task: data.task,
       minutesAmount: data.minutesAmount,
       startDate: new Date(),
-    }
+    };
 
-    setCycles((state) => [...state, newCycle])
+    setCycles((state) => [...state, newCycle]);
     setActiveCycleId(newCycle.id);
-    setAmountSecondsPassed(0)
+    setAmountSecondsPassed(0);
 
     reset();
   }
 
   // Time calc block
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-  const currentSecond = activeCycle ? totalSeconds - amountSecondsPassed : 0
-  const minutesAmount = Math.floor(currentSecond / 60)
-  const secondsAmount = currentSecond % 60
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSecond = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+  const minutesAmount = Math.floor(currentSecond / 60);
+  const secondsAmount = currentSecond % 60;
 
   // as variaveis minutes e seconds abaixo estão usando a função string com metodo
   // padStart para preencher os valores de segundos em caso de string com apenas um caracter
-  const minutes = String(minutesAmount).padStart(2, '0')
-  const seconds = String(secondsAmount).padStart(2, '0')
+  const minutes = String(minutesAmount).padStart(2, "0");
+  const seconds = String(secondsAmount).padStart(2, "0");
   // Time calc block
 
-  const task = watch('task')
-  const submitButtonIsDisabled = !task
+  // Countdown no Favicon do Navegador
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `Contador do Varela - ${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
+  // Countdown no Favicon do Navegador
+
+  const task = watch("task");
+  const submitButtonIsDisabled = !task;
 
   return (
     <HomeContainer>
